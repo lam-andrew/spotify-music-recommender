27ec164cadce0dd19e037298f88699732315af7f
lam-andrew <andrewlam2193@gmail.com>
2024-03-10 16:28:52 -0700

    react tsx project creation

---
+++ music-recommender/api/hello.ts
@@ -0,0 +1,5 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default (req: VercelRequest, res: VercelResponse): void => {
+  res.status(200).json({ message: 'Hello from Vercel!' });
+};
