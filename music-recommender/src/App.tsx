c1122276d72075e86637cf9f8f90cb6be3d9ab25
lam-andrew <andrewlam2193@gmail.com>
2024-03-10 22:45:08 -0700

    fixed routes

--- music-recommender/src/App.tsx
+++ music-recommender/src/App.tsx
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // Import your page components
 import HomePage from './pages/HomePage'
 import NotFoundPage from './pages/NotFoundPage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Set HomePage as the default by associating it with the root path */}
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/homepage" element={<HomePage />} />
         {/* Catch-all route for 404 Not Found pages */}
         <Route path="*" element={<NotFoundPage />} />
