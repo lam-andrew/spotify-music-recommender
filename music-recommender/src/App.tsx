3a8327d757dbb3632fe2ae7fcc807eaf6124a784
lam-andrew <andrewlam2193@gmail.com>
2024-03-13 21:50:06 -0700

    navbar

--- music-recommender/src/App.tsx
+++ music-recommender/src/App.tsx
@@ -1,27 +1,37 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 
 // Import your page components
-import HomePage from './pages/HomePage'
-import NotFoundPage from './pages/NotFoundPage'
-import LoginPage from './pages/LoginPage'
-import CallbackPage from './pages/CallbackPage'
-import ProfilePage from './pages/ProfilePage'
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import CallbackPage from './pages/CallbackPage';
+import ProfilePage from './pages/ProfilePage';
 
-function App() {
+const AppWrapper = () => {
   return (
     <Router>
+      <App />
+    </Router>
+  );
+};
+
+const App = () => {
+  const location = useLocation(); // Hook to get the current location
+  
+  return (
+    <>
+      {/* Conditionally render NavigationBar */}
+      {location.pathname !== '/' && <NavigationBar />}
       <Routes>
-        {/* Set HomePage as the default by associating it with the root path */}
         <Route path="/" element={<LoginPage />} />
         <Route path="/callback" element={<CallbackPage />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/profilepage" element={<ProfilePage />} />
-        {/* Catch-all route for 404 Not Found pages */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </Router>
-  )
-}
+    </>
+  );
+};
 
-export default App
+export default AppWrapper;
