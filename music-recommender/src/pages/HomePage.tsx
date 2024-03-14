117e2a77918545560619d8dc5bc4d0c6ed60a54f
lam-andrew <andrewlam2193@gmail.com>
2024-03-13 22:50:32 -0700

    2 col grid

--- music-recommender/src/pages/HomePage.tsx
+++ music-recommender/src/pages/HomePage.tsx
@@ -112,7 +112,7 @@ const HomePage = () => {
         <h2 className="mt-8 text-2xl font-bold mb-2">Search Results</h2>
         <div className="bg-zinc-900 p-6 rounded-lg shadow-lg overflow-auto">
           {searchResults.length > 0 ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
               {searchResults.map((track, index) => (
                 <div 
                   key={index} 
