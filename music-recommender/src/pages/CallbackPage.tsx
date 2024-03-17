1499299059a4af7046042a012d75e0a1931ed220
lam-andrew <andrewlam2193@gmail.com>
2024-03-16 17:22:26 -0700

    test

--- music-recommender/src/pages/CallbackPage.tsx
+++ music-recommender/src/pages/CallbackPage.tsx
@@ -45,7 +45,7 @@ const CallbackPage = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-light-blue"></div>
+      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-light-blue"></div>
     </div>
   )
 }
